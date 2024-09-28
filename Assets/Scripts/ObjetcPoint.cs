816263ea8442c0a1a5b49283d6cb684f9ae0b9b1
Franco Busch <francobusch130@gmail.com>
2024-09-28 03:00:56 -0300

    Fixea sonidos

--- Assets/Scripts/ObjetcPoint.cs
+++ Assets/Scripts/ObjetcPoint.cs
@@ -30,21 +30,21 @@ public class ObjetcPoint : MonoBehaviour
     void OnDestroy()
     {
         if (gameObject.CompareTag("Objetive"))
-        {
-            Score.objetive++;
-            AudioSource.PlayClipAtPoint(destructionSound, transform.position);
-        }
-        else if (gameObject.CompareTag("Score") && EndGame.Contador == true)
-        {
-            Score.score += 100;
-            AudioSource.PlayClipAtPoint(destructionSound, transform.position);
-
-        } else if (gameObject.CompareTag("Bonus"))
-        {
-            Timer.elapsedTime -= 10f;
-            ContadorTiempo.tiempoTranscurrido -= 10f;
-            AudioSource.PlayClipAtPoint(destructionSound, transform.position);
-        }
+{
+    Score.objetive++;
+    AudioSource.PlayClipAtPoint(destructionSound, transform.position, 0.1f); // Volumen a 0.3
+}
+else if (gameObject.CompareTag("Score") && EndGame.Contador == true)
+{
+    Score.score += 100;
+    AudioSource.PlayClipAtPoint(destructionSound, transform.position, 0.1f); // Volumen a 0.3
+}
+else if (gameObject.CompareTag("Bonus"))
+{
+    Timer.elapsedTime -= 10f;
+    ContadorTiempo.tiempoTranscurrido -= 10f;
+    AudioSource.PlayClipAtPoint(destructionSound, transform.position, 0.1f); // Volumen a 0.3
+}
     }
 
     public static void CheckRecordScore()
